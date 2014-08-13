f3f923c2a1c00f2ad7ee7021c7a1463580e28a09
aziflaj <aldoziflaj95@gmail.com>
2014-08-12 18:18:46 -0700

    showing AlertDialog to insert a task

--- src/com/example/TodoList/MainActivity.java
+++ src/com/example/TodoList/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.TodoList;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 	/**
@@ -26,7 +29,21 @@ public class MainActivity extends Activity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 			case R.id.action_add_task:
-				Log.d("MainActivity","Add a new task");
+				AlertDialog.Builder builder = new AlertDialog.Builder(this);
+				builder.setTitle("Add a task");
+				builder.setMessage("What do you want to do?");
+				final EditText inputField = new EditText(this);
+				builder.setView(inputField);
+				builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialogInterface, int i) {
+						Log.d("MainActivity",inputField.getText().toString());
+					}
+				});
+
+				builder.setNegativeButton("Cancel",null);
+
+				builder.create().show();
 				return true;
 
 			default:
