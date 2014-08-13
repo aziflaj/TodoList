526902fe77156130ab203481c47726b5c5ec20ed
aziflaj <aldoziflaj95@gmail.com>
2014-08-12 17:59:46 -0700

    added menu

--- src/com/example/TodoList/MainActivity.java
+++ src/com/example/TodoList/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.TodoList;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 	/**
@@ -11,5 +14,23 @@ public class MainActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu,menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.action_add_task:
+				Log.d("MainActivity","Add a new task");
+				return true;
+
+			default:
+				return false;
+		}
 	}
 }
